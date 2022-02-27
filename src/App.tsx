0a640ae6952f9336f7e1a8d09d1d9153b9278a92
anass.tl <anass.sadik@taillislabs.com>
2022-02-27 16:55:38 +0100

    project clean up and theme config

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import GlobalStyle from './styles/global';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+const App: React.FC = () => (
+  <>
+    <ThemeProvider theme={theme} />
+    <GlobalStyle />
+  </>
+);
 export default App;
---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+export const theme = {
+  colors: {
+    dark_grey: '#1E1E1E',
+    black: '#525252',
+    medium_grey: '#3D3D3D',
+    shadow_gold: '#302D28',
+    yellow: '#FED530',
+    title: '#2E2E2E',
+    body: '#808080',
+    field: '#CCCCCC',
+    disabled: '#E6E6E6',
+    white: '#FFFFFF',
+    dark: '#000000',
+    image_placeholder: '#333333',
+  },
+};
