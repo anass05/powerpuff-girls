745787755727d2d2971d45b70ad1f78d112df543
anass.tl <anass.sadik@taillislabs.com>
2022-03-01 22:04:44 +0100

    changed Readme and .env file

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,11 +1,14 @@
+console.log(process.env);
+export const { REACT_APP_SHOW_ID } = process.env;
+
 export const URLS = {
   avatar: 'https://avatars.dicebear.com/api/micah/john.svg',
 };
 
 export const API_URIS = {
   base_url: 'https://api.tvmaze.com/',
-  details: 'shows/6771',
-  episodes: 'shows/6771/episodes',
+  details: `shows/${REACT_APP_SHOW_ID}`,
+  episodes: `shows/${REACT_APP_SHOW_ID}/episodes`,
 };
 
 export const routes = {
